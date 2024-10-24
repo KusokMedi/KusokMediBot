275c5b2c74fa53b89bd6edc3b5b3a7be3c4bf2ca
KusokMedi <matvejs.upesleja116@gmail.com>
2024-10-24 19:52:00 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,22 @@
+import telebot as t
+import requests
+import json
+
+bot = t.TeleBot("7597352034:AAERRI9ifzhXbaRiyeOOWWHxjvDDixzU8Cs")
+WeatherAPI = "de4d5b42cc1c310d27e1c75beca01647"
+print("Bot successfully started!")
+@bot.message_handler(commands=["start", "restart"])
+def start(message):
+    bot.send_message(message.chat.id, f"Привет {message.chat.first_name}. Напиши название города\nи я скажу тебе какая там температура 🌡")
+
+@bot.message_handler(content_types=["text"])
+def getw(message):
+    try:
+        city = message.text.strip().lower()
+        res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WeatherAPI}&units=metric")
+        data = json.loads(res.text)
+        bot.send_message(message.chat.id, f"Сейчас в городе {data["main"]["temp"]} градуса.\nЕсли хотите узнать про другой город, напиши мне!")
+    except Exception:
+        bot.send_message(message.chat.id, "Неверный город, введите город заново")
+        bot.register_next_step_handler(message, getw)
+bot.polling(none_stop=True)
